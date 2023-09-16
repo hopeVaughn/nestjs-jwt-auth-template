226e3778529fdba00f4a9f82da42f99610ff1f55
hopeVaughn <hopevaughnwarren@gmail.com>
2023-09-16 08:40:16 -0700

    finish logic for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,33 +2,34 @@ import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
-import { AtGuard } from 'src/common/guards/at.guard';
 import { RtGuard } from 'src/common/guards';
-import { GetCurrentUserId, GetCurrentUser } from 'src/common/decorators';
+import { GetCurrentUserId, GetCurrentUser, Public } from 'src/common/decorators';
 @Controller('auth')
 export class AuthController {
 
   constructor (private authService: AuthService) { }
 
+  @Public()
   @Post('/local/signup')
   @HttpCode(HttpStatus.CREATED)
   signuplocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signuplocal(dto);
   }
 
+  @Public()
   @Post('/local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signinLocal(dto);
   }
 
-  @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() userId: string) {
     return this.authService.logout(userId);
   }
 
+  @Public()
   @UseGuards(RtGuard)
   @Post('/refresh')
   @HttpCode(HttpStatus.OK)
